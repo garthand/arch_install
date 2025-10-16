9b4fc877652b00f1988678ce0815f9a52b3a5800
garthand <garthand@users.noreply.github.com>
2025-10-16 16:56:04 -0500

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -13,7 +13,7 @@ mkfs.btrfs -L archlinux /dev/mapper/cryptroot
 mount /dev/mapper/cryptroot /mnt
 mkfs.fat -F 32 /dev/sda1
 mount -o umask=0077 --mkdir /dev/sda1 /mnt/boot
-pacstrap -K /mnt base linux linux-firmware systemd-ukify vim amd-ucode man-db man-pages texinfo sof-firmware btrfs-progs cryptsetup sbctl dracut sudo systemd-homed
+pacstrap -K /mnt base linux linux-firmware systemd-ukify vim amd-ucode man-db man-pages texinfo sof-firmware btrfs-progs cryptsetup sbctl dracut sudo systemd-homed zram-generator
 ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
@@ -56,6 +56,12 @@ Exec = /bin/sh -c 'while read -r f; do /usr/lib/systemd/systemd-sbsign sign --pr
 Depends = sh
 NeedsTargets
 EOF
+cat << EOF > /etc/systemd/zram-generator.conf
+[zram0]
+zram-size = min(ram / 2, 4096)
+compression-algorithm = zstd
+EOF
+systemctl enable systemd-zram-setup@zram0.service
 sbctl create-keys
 sbctl enroll-keys -m
 cp /var/lib/sbctl/keys/db/db.key /etc/kernel/secure-boot-private-key.pem
