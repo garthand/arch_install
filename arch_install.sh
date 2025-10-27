f12b0e638b3c549682d4a10e15c731c2a6226976
garthand <garthand@users.noreply.github.com>
2025-10-27 18:28:16 -0500

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -15,7 +15,7 @@ mkfs.fat -F 32 /dev/sda1
 mount -o umask=0077 --mkdir /dev/sda1 /mnt/boot
 # For nvidia: linux-headers nvidia-open-dkms nvidia-utils lib32-nvidia-utils linux-firmware-nvidia
 # For AMD: vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa linux-firmware-amdgpu
-pacstrap -K /mnt base linux linux-firmware systemd-ukify vim amd-ucode man-db man-pages texinfo sof-firmware btrfs-progs cryptsetup sbctl dracut sudo zram-generator rpcbind which gnome xorg-xwayland vulkan-tools steam gamemode lib32-gamemode lutris bazaar
+pacstrap -K /mnt base linux linux-firmware systemd-ukify vim amd-ucode man-db man-pages texinfo sof-firmware btrfs-progs cryptsetup sbctl dracut sudo zram-generator rpcbind which gnome xorg-xwayland vulkan-tools steam gamemode lib32-gamemode lutris flatpak
 ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 arch-chroot /mnt
 systemctl enable fstrim.timer
@@ -85,3 +85,12 @@ echo "alias ll='ls -l' 2>/dev/null" > /etc/profile.d/ll.sh
 #homectl create testuser --storage=directory --group=testuser --member-of=wheel --shell=/bin/bash --real-name="Test User" --password
 pacman -Rns --noconfirm orca
 useradd testuser -m -G wheel -s "/bin/bash" -c "Test User" -p $(openssl passwd -6 "password")
+# Symlink Steam Proton to Heroic Games Launcher
+flatpak install -y com.heroicgameslauncher.hgl
+ln -s ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/ ~/.var/app/com.heroicgameslauncher.hgl/config/heroic/tools/proton/Steam-Proton-Experimental
+# Symlink GE Proton to Steam
+ln -s ~/.var/app/com.heroicgameslauncher.hgl/config/heroic/tools/proton/GE-Proton-latest/ ~/.steam/steam/compatibilitytools.d/GE-Proton-latest
+# Install the right version of gamescope for Heroic Games Launcher
+heroic_runtime=$(flatpak list --columns=application,runtime|grep heroic|awk -F '/' '{print $3}')
+flatpak install -y flathub org.freedesktop.Platform.VulkanLayer.gamescope//"$heroic_runtime"
+flatpak install -y com.discordapp.Discord/x86_64/stable
