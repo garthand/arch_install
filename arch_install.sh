73ac04ae0d6f258221e2e0ffefa3183e1b9975a0
garthand <garthand@users.noreply.github.com>
2025-11-13 11:58:28 -0600

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -75,6 +75,16 @@ cp /var/lib/sbctl/keys/db/db.key /etc/kernel/secure-boot-private-key.pem
 cp /var/lib/sbctl/keys/db/db.pem /etc/kernel/secure-boot-certificate.pem
 pacman -Rns --noconfirm sbctl
 rm -rf /var/lib/sbctl
+drive=$(lsblk|grep -B 1 crypt|head -1|awk -F '─' '{print $2}'|awk '{print $1}')
+sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 /dev/"$drive"
+mkdir /etc/crypttab.d
+device_name=$(sudo awk '{print $1}' /etc/crypttab)
+device_uuid=$(sudo awk '{print $2}' /etc/crypttab)
+cat << EOF > /etc/crypttab.d/"${device_name}".conf
+NAME=$device_name
+DEVICE=$device_uuid
+OPTIONS=discard,tpm2-device=auto,x-initrd.attach
+EOF
 uuid=$(blkid|grep LUKS|awk -F '"' '{print $2}')
 echo "cryptroot UUID=$uuid none discard" > /etc/crypttab
 kernel_version=$(ls /usr/lib/modules)
