6079237ab3b6229f1170e994c4b8dc33165718fd
garthand <garthand@users.noreply.github.com>
2025-11-17 19:21:43 -0600

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -17,11 +17,11 @@ mount -o umask=0077 --mkdir /dev/sda1 /mnt/boot
 # For AMD: vulkan-radeon lib32-vulkan-radeon linux-firmware-amdgpu
 sed -i '/^#\[multilib\]$/ {n; s/.*/Include = \/etc\/pacman\.d\/mirrorlist/}' /etc/pacman.conf
 sed -i 's/^#\[multilib\]/[multilib]/' /etc/pacman.conf
-sed -i '/^#\[multilib\]$/ {n; s/.*/Include = \/etc\/pacman\.d\/mirrorlist/}' /mnt/etc/pacman.conf
-sed -i 's/^#\[multilib\]/[multilib]/' /mnt/etc/pacman.conf
 pacstrap -K /mnt base base-devel git linux linux-firmware systemd-ukify vim amd-ucode man-db man-pages texinfo sof-firmware btrfs-progs cryptsetup sbctl dracut sudo zram-generator rpcbind which xorg-xwayland vulkan-tools steam gamemode lib32-gamemode lutris flatpak dash firewalld dash firefox libreoffice-fresh mesa lib32-mesa pipewire wireplumber networkmanager plasma-meta konsole dolphin kate skanpage gwenview plasma-systemmonitor khelpcenter sweeper partitionmanager kolourpaint ksystemlog isoimagewriter ktorrent ark kcalc spectacle hunspell hunspell-en_us 
 ln -sf ../run/NetworkManager/resolv.conf /mnt/etc/resolv.conf
 arch-chroot /mnt
+sed -i '/^#\[multilib\]$/ {n; s/.*/Include = \/etc\/pacman\.d\/mirrorlist/}' /mnt/etc/pacman.conf
+sed -i 's/^#\[multilib\]/[multilib]/' /mnt/etc/pacman.conf
 systemctl enable fstrim.timer
 systemctl enable systemd-oomd
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
